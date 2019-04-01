c8cfa216967f104451ebdee44e289816abb25b98
Kapokopen <963041275@qq.com>
2019-04-01 11:07:34 +0800

    第三章 项目准备

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <span class="text">ABCDEFG</span>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  const getters = {
+    a: () => 1,
+    b: () => 2,
+    c: () => 3
+  }
+  function fn(keys) {
+    const data = {}
+    keys.forEach(key => {
+      if (getters.hasOwnProperty(key)) {
+        data[key] = getters[key]
+      }
+    })
+  }
+  export default {
+    computed: {
+      ...mapGetters(['test']),
+      ...fn(['a', 'b', 'c'])
+    },
+    mounted() {
+      // this.$store.dispatch('setTest', 9).then(() => {
+      //   console.log(this.$store.state.book.test)
+      // })
+      console.log(this.a, this.b, this.c)
+    }
+  }
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > 50 ? 50 : fontSize
+    html.style.fontSize = fontSize + 'px'
+  })
+</script>
+
+<style lang="scss" scoped>
+  @import "./assets/styles/global";
+  .text {
+    font-family: 'Days One';
+    font-size: px2rem(20);
+    color: orange;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <span class="icon-bookmark"></span>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scope>
+  .icon-bookmark {
+    font-size: 20px;
+    color: red;
+  }
+</style>
